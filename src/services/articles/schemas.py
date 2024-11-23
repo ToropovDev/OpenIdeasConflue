da30ef90fa35956f58dbbb5367c177f0900cf03a
Торопов Арсений Алексеевич <toropov_a@tochka.com>
2024-11-23 21:40:57 +0500

    feat: SCRUM-17 - cr 0.1

--- src/services/articles/schemas.py
+++ src/services/articles/schemas.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from pickle import FALSE
+from typing import Any
 
 from pydantic import BaseModel, ConfigDict
 
@@ -8,8 +10,12 @@ class Article(BaseModel):
         extra="ignore",
     )
 
+    name: str
     text: str
+    section: str = ""
     created_at: datetime = datetime.now()
+    tags: dict[str, Any] = {}
+    is_draft: bool = True
 
 
 class UpdateArticle(BaseModel):
@@ -17,4 +23,7 @@ class UpdateArticle(BaseModel):
         extra="ignore",
     )
 
+    name: str
     text: str
+    tags: dict[str, Any] = {}
+    is_draft: bool = False

33750008f7f559621d8e427105343b21d3c0a081
Торопов Арсений Алексеевич <toropov_a@tochka.com>
2024-11-23 23:03:01 +0500

    feat: SCRUM-22 - add get_file api

--- src/services/files/methods.py
+++ src/services/files/methods.py
@@ -1,6 +1,10 @@
+import uuid
+from src.db.base import connect as db_connect
 from fastapi import APIRouter, UploadFile, File
 
 from src import responses
+from src.db.queries.file_article import add_to_article
+from src.db.queries.files import get_file as _get_file
 from src.services.files.s3_client import upload
 
 router = APIRouter(prefix="/files", tags=["files"])
@@ -17,5 +21,19 @@ async def upload_file(
     return responses.OK(
         content={
             "details": url,
+        },
+    )
+
+
+@router.get("/{file_id}")
+async def get_file(file_id: uuid.UUID):
+    async with db_connect() as conn:
+        file = await _get_file(conn, file_id)
+
+    return responses.OK(
+        content={
+            "details": {
+                "file": file.model_dump(mode="json"),
+            },
         },
     )

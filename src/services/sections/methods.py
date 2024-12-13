34cd4bb5e4b75289db6a7f7adeba26bac335c324
ToropovDev <arseneyt@yandex.ru>
2024-12-14 00:48:02 +0500

    fix: SCRUM-19 - fix create method

--- src/services/sections/methods.py
+++ src/services/sections/methods.py
@@ -23,7 +23,7 @@ async def create_section(
     section: Section = Depends(Section),
 ) -> JSONResponse:
     async with db_connect() as conn:
-        created_section = await _create_section(
+        created_section_id = await _create_section(
             conn=conn,
             section=section,
         )
@@ -31,7 +31,7 @@ async def create_section(
     return responses.OK(
         content={
             "details": None,
-            "section_id": created_section.id,
+            "section_id": str(created_section_id),
         },
     )
 

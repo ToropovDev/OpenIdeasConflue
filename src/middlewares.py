3d74ad41cc0a108ba8a7f5b023cc8698dc22cfbc
ToropovDev <arseneyt@yandex.ru>
2025-01-07 12:08:45 +0500

    feat: SCRUM-23 - template for auth

---
+++ src/middlewares.py
@@ -0,0 +1,13 @@
+import typing as t
+
+from fastapi import Request
+from fastapi import Response
+
+async def auth(
+    request: Request,
+    call_next: t.Callable[[Request], t.Awaitable[Response]],
+) -> Response:
+
+    # TODO: handle auth here
+
+    return await call_next(request)
---
+++ src/services/auth/methods.py
@@ -0,0 +1,1 @@
+# TODO: make auth in and out methods here
